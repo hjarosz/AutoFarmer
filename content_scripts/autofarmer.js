34827650be448652452ca8bfa0ed13b156bedc77
grubaz <van.huberto@gmail.com>
2023-03-30 17:34:39 +0200

    fix + delay

--- content_scripts/autofarmer.js
+++ content_scripts/autofarmer.js
@@ -4,7 +4,11 @@
   }
   window.hasRun = true;
 
-  function plant(size) {
+  const delay = millis => new Promise((resolve, reject) => {
+    setTimeout(_ => resolve(), millis)
+  });
+
+  async function plant(size) {
     var fieldNumber = 0;
     var horizontalIncrement = 1;
     var verticalIncrement = 1;
@@ -19,22 +23,26 @@
     for (var j = 0; j <= 9; j = j + verticalIncrement) {
       for (var i = 1; i <= 12; i = i + horizontalIncrement) {
         fieldNumber = (j * 12) + i;
-        var field = document.getElementById('f' + i)
+        var field = document.getElementById('f' + fieldNumber)
         if (field == null)
           continue
         field.click();
+        await delay(10);
       }
     }
 
+    await delay(500);
     document.getElementById("giessen").click();
+    await delay(500);
 
     for (var j = 0; j <= 9; j = j + verticalIncrement) {
       for (var i = 1; i <= 12; i = i + horizontalIncrement) {
         fieldNumber = (j * 12) + i;
-        var field = document.getElementById('f' + i)
+        var field = document.getElementById('f' + fieldNumber)
         if (field == null)
           continue
         field.click();
+        await delay(10);
       }
     }
   }
